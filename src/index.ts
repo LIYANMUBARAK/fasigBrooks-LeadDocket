16653837960dbea947d46d64e5b999702658af2e
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2024-10-08 23:16:50 +0530

    environment setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, {  Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import router from './routes/routes';
+
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/',router)
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
